ae48b74fc504883a954e3fe7d393a1482ae917f6
@LeonidQWE <lenya.tomashevich.forps@mail.ru>
2024-05-14 20:59:55 +0300

    add title

--- project/src/App/index.tsx
+++ project/src/App/index.tsx
@@ -1,7 +1,11 @@
+import PageTitle from '../components/PageTitle'
+
+import styles from './App.module.css'
+
 export default function App() {
   return (
-    <>
-
-    </>
+    <div className={styles.app}>
+      <PageTitle>Todo</PageTitle>
+    </div>
   )
 }
---
+++ project/src/components/PageTitle/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import styles from './PageTitle.module.css'
+
+type PageTitleProps = {
+  children: string;
+}
+
+const PageTitle: React.FC<PageTitleProps> = ({ children }) => {
+  return <h1 className={styles.pageTitle}>{children}</h1>;
+}
+
+export default PageTitle
